8afc1607921484f5dd5518d7774f7f7734717f46
潘允蕙 <maypan1107@gmail.com>
2025-08-16 20:42:40 +0800

    edit discription of the website

--- astro-nano-main/src/consts.ts
+++ astro-nano-main/src/consts.ts
@@ -9,7 +9,7 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  DESCRIPTION: "Yun-Huei Pan's personal website.",
 };
 
 export const BLOG: Metadata = {
